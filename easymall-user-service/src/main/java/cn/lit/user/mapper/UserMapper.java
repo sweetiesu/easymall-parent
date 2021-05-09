e08955f476ea1de11134e7271bc1e1a1a4106976
sweetiesu <2357516152@qq.com>
2021-05-09 13:54:01 +0800

    查询、插入用户信息

---
+++ easymall-user-service/src/main/java/cn/lit/user/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package cn.lit.user.mapper;
+
+import com.jt.common.pojo.User;
+
+public interface UserMapper {
+    int selectUserCountByUserName(String userName);
+
+    void insertUser(User user);
+
+    User selectUserByUserNameAndPassword(User user);
+}
